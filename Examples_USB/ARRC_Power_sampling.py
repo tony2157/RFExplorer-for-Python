7a011cafbb17221ab5a87bf5c24d781074997c60
tony2157 <tony.segales700@gmail.com>
2022-06-29 17:58:55 -0500

    More changes

--- Examples_USB/ARRC_Power_sampling.py
+++ Examples_USB/ARRC_Power_sampling.py
@@ -36,11 +36,14 @@ def PrintPeak(objAnalazyer):
     # Create array with values to send over mavlink
     values = [fCenterFreq, fAmplitudeDBM, 0, 0, 0]
 
+    time.sleep(1)
+
     # MAVLink_arrc_sensor_raw_message(time_boot_ms, app_datatype, app_datalength, values)
     #ARRC_mav_connection.mav.send(ARRCmavlink.MAVLink_arrc_sensor_raw_message(10,0,2,values))
     #ARRC_mav_connection.mav.send(mavutil.mavlink.MAVLink_arrc_sensor_raw_message(10,0,2,values))
-    ARRC_mav_connection.mav.arrc_sensor_raw_send(10,0,2,values)    
-    #print(str(values))
+    ARRC_mav_connection.mav.arrc_sensor_raw_send(10,0,2,values,True)    
+    ARRC_msg = mavutil.mavlink.MAVLink_arrc_sensor_raw_message(10,0,2,values)
+    print(str(ARRC_msg))
 
     #msg = ARRC_mav_connection.recv_match(blocking=True)
     #print(str(msg))
@@ -84,14 +87,14 @@ def ControlSettings(objAnalazyer):
 #---------------------------------------------------------
 # global variables and initialization
 #---------------------------------------------------------
-ARRC_mav_connection = mavutil.mavlink_connection('/dev/ttyAMA0', baud=57600, source_system=1, source_component=191) #'udpin:127.0.0.1:14551'
+ARRC_mav_connection = mavutil.mavlink_connection('/dev/serial0', baud=19200, source_system=1, source_component=191) #'udpin:127.0.0.1:14551'
 
 yay = ARRC_mav_connection.wait_heartbeat()
 print("Heartbeat system: sysID %u compID %u" % (ARRC_mav_connection.target_system, ARRC_mav_connection.target_component))
 
-ARRC_mav_connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_GENERIC, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
+ARRC_mav_connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
 
-ARRC_mav_connection.mav.request_data_stream_send(ARRC_mav_connection.target_system, ARRC_mav_connection.target_component,mavutil.mavlink.MAV_DATA_STREAM_ALL,1,1)
+#ARRC_mav_connection.mav.request_data_stream_send(ARRC_mav_connection.target_system, ARRC_mav_connection.target_component,mavutil.mavlink.MAV_DATA_STREAM_ALL,1,1)
 print("Mavlink connection: "+ str(yay))
 
 SERIALPORT = None    #serial port identifier, use None to autodetect  
