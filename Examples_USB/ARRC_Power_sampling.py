4a6718d405e2e9f753be3b6ddd292e85898ee472
tony2157 <tony.segales700@gmail.com>
2022-07-01 10:04:30 -0500

    Remove useless functions and comments

--- Examples_USB/ARRC_Power_sampling.py
+++ Examples_USB/ARRC_Power_sampling.py
@@ -34,20 +34,7 @@ def PrintPeak(objAnalazyer):
 
     #print("     Peak: " + "{0:.3f}".format(fCenterFreq) + "MHz  " + str(fAmplitudeDBM) + "dBm")
 
-    # Create array with values to send over mavlink
-    #values = [fCenterFreq, fAmplitudeDBM, 0, 0, 0]
-
-    time.sleep(1)
-
-    # MAVLink_arrc_sensor_raw_message(time_boot_ms, app_datatype, app_datalength, values)
-    #ARRC_mav_connection.mav.send(ARRCmavlink.MAVLink_arrc_sensor_raw_message(10,0,2,values))
-    #ARRC_mav_connection.mav.send(mavutil.mavlink.MAVLink_arrc_sensor_raw_message(10,0,2,values))
-    ARRC_mav_connection.mav.arrc_sensor_raw_send(10,0,fCenterFreq,fAmplitudeDBM)    
-    ARRC_msg = mavutil.mavlink.MAVLink_arrc_sensor_raw_message(10,0,fCenterFreq,fAmplitudeDBM)
-    print(str(ARRC_msg))
-
-    #msg = ARRC_mav_connection.recv_match(blocking=True)
-    #print(str(msg))
+    ARRC_mav_connection.mav.arrc_sensor_raw_send(10,0,fCenterFreq,fAmplitudeDBM)
 
 
 def ControlSettings(objAnalazyer):
