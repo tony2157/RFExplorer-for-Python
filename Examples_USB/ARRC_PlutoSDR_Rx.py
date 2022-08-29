828bef02e8e52c1d3ce3971946b7050a6e5e7d4a
tony2157 <tony.segales700@gmail.com>
2022-08-29 17:16:46 -0500

    Pluto: code changes

--- Examples_USB/ARRC_PlutoSDR_Rx.py
+++ Examples_USB/ARRC_PlutoSDR_Rx.py
@@ -16,27 +16,30 @@ ARRC_mav_connection = mavutil.mavserial('/dev/serial0', baud=115200, source_syst
 
 # Wait for hearbeat from Pixhawk
 PX4_beat = ARRC_mav_connection.wait_heartbeat(timeout=10)
-print("Mavlink connection: "+ str(PX4_beat))
-print("Heartbeat system: sysID %u compID %u" % (ARRC_mav_connection.target_system, ARRC_mav_connection.target_component))
+if (PX4_beat != None):
+    print("Mavlink connection: "+ str(PX4_beat))
+    print("Heartbeat system: sysID %u compID %u" % (ARRC_mav_connection.target_system, ARRC_mav_connection.target_component))
 
-# Send RPi heartbeat to confirm handshake
-ARRC_mav_connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
+    # Send RPi heartbeat to confirm handshake
+    ARRC_mav_connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
 
-# Wait for config message 
-msg = ARRC_mav_connection.recv_match(type='ARRC_SENSOR_RAW', blocking=True)
-if not msg:
-    DFREQ = 3070
-elif msg.get_type() == "BAD_DATA":
-    DFREQ = 3070
+    # Wait for config message 
+    msg = ARRC_mav_connection.recv_match(type='ARRC_SENSOR_RAW', blocking=True)
+    if not msg:
+        DFREQ = 3070
+    elif msg.get_type() == "BAD_DATA":
+        DFREQ = 3070
+    else:
+        DFREQ = msg.dfreq
 else:
-    DFREQ = msg.dfreq
+    DFREQ = 3070
 
 sample_rate = 1e6 # Hz
-center_freq = DFREQ*1000000
+center_freq = DFREQ*1e6
 num_samps = 1024 # number of samples returned per call to rx()
 
 sdr.gain_control_mode_chan0 = 'manual'
-sdr.rx_hardwaregain_chan0 = 47.0 # dB
+sdr.rx_hardwaregain_chan0 = 0.0 # dB
 sdr.rx_lo = int(center_freq)
 sdr.sample_rate = int(sample_rate)
 sdr.rx_rf_bandwidth = int(sample_rate) # filter width, just set it to the same as sample rate for now
@@ -50,7 +53,7 @@ while (True):
     IQ_dfreq = samples[511]
     print(IQ_dfreq)
 
-    pwr_dB = 20*math.log10(math.sqrt(IQ_dfreq.real**2 + IQ_dfreq.imag**2))
+    pwr_dB = 10*math.log10(abs(IQ_dfreq))
 
     # Put code to save data to Pi's SD card here
 
